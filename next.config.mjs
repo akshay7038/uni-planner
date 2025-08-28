df8c85f6f94d262b15a38f1608798f164a0dbf3c
Akshay khandare <45727139+akshay7038@users.noreply.github.com>
2025-08-28 21:04:37 +1000

    commit test

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,5 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
+const nextConfig = { 
   reactStrictMode: true,
-  experimental: { appDir: true }
 };
-const nextConfig = { reactStrictMode: true };
 export default nextConfig;
