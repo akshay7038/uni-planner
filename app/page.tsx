1092ddb13657da778469bdd8e0ef1ff9423777cf
Akshay khandare <45727139+akshay7038@users.noreply.github.com>
2025-08-29 22:58:55 +1000

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,3 @@
 import dynamic from "next/dynamic";
-
 const ClientPlanner = dynamic(() => import("../components/ClientPlanner"), { ssr: false });
-
-export default function Page() {
-  return <ClientPlanner />;
-}
+export default function Page() { return <ClientPlanner />; }
